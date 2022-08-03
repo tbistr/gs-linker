ebffa1d5af1a6b3f173c49fd9699555a5216d6e4
tbistr <tbistr99@gmail.com>
2022-08-03 12:45:24 +0000

    build: go mod vendor

--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/tbistr/gs-linker
 go 1.17
 
 require (
-	github.com/google/go-github/v43 v43.0.0
+	github.com/bradleyfalzon/ghinstallation v1.1.1
+	github.com/caarlos0/env/v6 v6.9.3
+	github.com/google/go-github/v45 v45.0.0
 	github.com/k0kubun/pp v3.0.1+incompatible
-	github.com/slack-go/slack v0.10.2
+	github.com/k0kubun/pp/v3 v3.1.0
+	github.com/slack-go/slack v0.11.2
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/go-github/v29 v29.0.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
@@ -16,4 +21,5 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/tbistr/gs-linker/gs"
+	"github.com/caarlos0/env/v6"
+	"github.com/k0kubun/pp/v3"
+	gh "github.com/tbistr/gs-linker/github"
+	sl "github.com/tbistr/gs-linker/slack"
 )
 
-func main() {
-	linker := gs.New()
+type envs struct {
+	GhAppID          int64  `env:"GH_APP_ID"`
+	GhInstallationID int64  `env:"GH_INSTALLATION_ID"`
+	SlToken          string `env:"SL_TOKEN"`
+	SlSigningSecret  string `env:"SL_SIGNING_SECRET"`
+}
 
-	http.HandleFunc("/github/events", linker.HandleGithubEvent())
-	http.HandleFunc("/slack/events", linker.HandleSlackEvent())
+func main() {
+	e := envs{}
+	if err := env.Parse(&e); err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("Get envs: %+v\n", e)
+
+	ghClient := gh.New(e.GhAppID, e.GhInstallationID)
+	slClient := sl.New(e.SlToken, e.SlSigningSecret)
+
+	var (
+		// onIssueCommented gh.OnIssueCommentedFunc = func(client *gh.Client, owner, repo string, num int, comment *github.IssueComment) error {
+		// 	return nil
+		// }
+		// onPrCommented gh.OnPrCommentedFunc = func(client *gh.Client, owner, repo string, num int, comment *github.PullRequestComment) error {
+		// 	return nil
+		// }
+
+		onMentioned sl.OnMentionedFunc = func(client *sl.Client, channel, threadTS, text string) error {
+			fmt.Println("Mentioned:")
+			pp.Println(channel)
+			pp.Println(threadTS)
+			pp.Println(text)
+			return nil
+		}
+		onMsgSent sl.OnMsgSentFunc = func(client *sl.Client, channel, threadTS, text string) error {
+			fmt.Println("MsgSent:")
+			pp.Println(threadTS)
+			pp.Println(text)
+			return nil
+		}
+	)
+	slClient.RegisterOnMentioned(onMentioned)
+	slClient.RegisterOnMsgSent(onMsgSent)
+
+	http.HandleFunc("/github/events", ghClient.HandleEvent())
+	http.HandleFunc("/slack/events", slClient.HandleEvent())
 
 	log.Println("[INFO] Server listening")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
