4866a7d631874c085809e24be742c67e2e326b91
tbistr <tbistr99@gmail.com>
2022-08-06 14:26:11 +0000

    feat: parse url

--- github/url.go
+++ github/url.go
@@ -1,51 +1,55 @@
 package gh
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/url"
 	"strconv"
 	"strings"
+
+	"github.com/google/go-github/v45/github"
 )
 
-func IsIssue(maybeURL *url.URL) (string, string, int, bool) {
-	if maybeURL.Host != "github.com" {
-		return "", "", 0, false
-	}
-
-	// assume that []string{"", "owner", "repo", "pull", "0"} or []string{"", "owner", "repo", "issues", "0"}
-	params := strings.Split(maybeURL.Path, "/")
-	if len(params) != 5 {
-		return "", "", 0, false
-	}
-	if params[3] != "issues" {
-		return "", "", 0, false
-	}
-
-	issueNum, err := strconv.Atoi(params[4])
+// VerifyURL parses and verifies the URL and returns an error if the URL is incorrect.
+// https://pkg.go.dev/github.com/google/go-github/v45@v45.0.0/github?utm_source=gopls#Issue
+// > Note: As far as the GitHub API is concerned, every pull request is an issue, but not every issue is a pull request. Some endpoints, events, and webhooks may also return pull requests via this struct.
+func (client *Client) VerifyURL(rawURL string) (*github.Issue, error) {
+	owner, repo, num, err := parseURL(rawURL)
 	if err != nil {
-		return "", "", 0, false
+		return nil, err
+	}
+	issue, res, err := client.github.Issues.Get(context.Background(), owner, repo, num)
+	if err != nil {
+		return nil, err
 	}
 
-	return params[1], params[2], issueNum, true
+	log.Printf("get issue info: %+v\n", res)
+	return issue, nil
 }
 
-func IsPR(maybeURL *url.URL) (string, string, int, bool) {
-	if maybeURL.Host != "github.com" {
-		return "", "", 0, false
+func parseURL(rawURL string) (owner, repo string, num int, err error) {
+	log.Printf("parsing URL: %#v\n", rawURL)
+	maybeURL, err := url.Parse(rawURL)
+	if err != nil {
+		return "", "", 0, err
 	}
 
-	// assume that []string{"", "owner", "repo", "pull", "0"} or []string{"", "owner", "repo", "issues", "0"}
+	if maybeURL.Host != "github.com" {
+		return "", "", 0, fmt.Errorf("%#v is not github url", maybeURL)
+	}
+
+	// assumes that []string{"", "owner", "repo", "pull", "0"} or []string{"", "owner", "repo", "issues", "0"}
 	params := strings.Split(maybeURL.Path, "/")
 	if len(params) != 5 {
-		return "", "", 0, false
+		return "", "", 0, fmt.Errorf("%#v is not assumed url", maybeURL)
 	}
-	if params[3] != "issues" {
-		return "", "", 0, false
-	}
-
-	issueNum, err := strconv.Atoi(params[4])
+	owner = params[1]
+	repo = params[2]
+	num, err = strconv.Atoi(params[4])
 	if err != nil {
-		return "", "", 0, false
+		return "", "", 0, err
 	}
 
-	return params[1], params[2], issueNum, true
+	return owner, repo, num, nil
 }
